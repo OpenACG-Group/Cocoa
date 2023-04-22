600782df52a6bb19d99beac3447cd7f4a4adc8ae
wait1210day <masshiro.io@qq.com>
2023-04-22 23:19:45 +0800

    feature: [Lottie] support AE expression evaluation

--- src/Gallium/bindings/lottie/AnimationBuilderWrap.cc
+++ src/Gallium/bindings/lottie/AnimationBuilderWrap.cc
@@ -144,6 +144,161 @@ private:
     v8::Global<v8::Function> func_;
 };
 
+template<typename T, typename ValueCvtFunctor>
+class JSExprEvaluatorImplT : public skottie::ExpressionEvaluator<T>
+{
+public:
+    JSExprEvaluatorImplT(v8::Isolate *isolate, v8::Local<v8::Function> func)
+        : isolate_(isolate), func_(isolate, func) {}
+
+    T evaluate(float t) override
+    {
+        v8::HandleScope scope(isolate_);
+        v8::Local<v8::Value> args[] = { v8::Number::New(isolate_, t) };
+
+        v8::Local<v8::Function> func = func_.Get(isolate_);
+        v8::Local<v8::Context> ctx = isolate_->GetCurrentContext();
+
+        v8::Local<v8::Value> ret;
+        if (!func->Call(ctx, v8::Null(isolate_), 1, args).ToLocal(&ret))
+            return ValueCvtFunctor::kFallbackV;
+
+        return value_cvt_(isolate_, ret);
+    }
+
+private:
+    v8::Isolate *isolate_;
+    v8::Global<v8::Function> func_;
+    ValueCvtFunctor value_cvt_;
+};
+
+struct NumberCvt
+{
+    static constexpr float kFallbackV = 0;
+
+    float operator()(v8::Isolate *i, v8::Local<v8::Value> v)
+    {
+        if (!v->IsNumber())
+            return kFallbackV;
+        return static_cast<float>(
+                v->NumberValue(i->GetCurrentContext()).ToChecked());
+    }
+};
+
+struct StringCvt
+{
+    static const SkString kFallbackV;
+    SkString operator()(v8::Isolate *i, v8::Local<v8::Value> v)
+    {
+        if (!v->IsString())
+            return kFallbackV;
+        v8::String::Utf8Value str(i, v);
+        return {*str, static_cast<size_t>(str.length())};
+    }
+};
+const SkString StringCvt::kFallbackV("");
+
+struct ArrayCvt
+{
+    static const std::vector<float> kFallbackV;
+    std::vector<float> operator()(v8::Isolate *i, v8::Local<v8::Value> v)
+    {
+        if (!v->IsArray())
+            return kFallbackV;
+
+        v8::Local<v8::Array> arr = v.As<v8::Array>();
+        std::vector<float> res(arr->Length());
+        for (uint32_t p = 0; p < arr->Length(); p++)
+        {
+            v8::Local<v8::Value> e;
+            if (!arr->Get(i->GetCurrentContext(), p).ToLocal(&e))
+                return kFallbackV;
+            if (!e->IsNumber())
+                return kFallbackV;
+            res[p] = static_cast<float>(
+                    e->NumberValue(i->GetCurrentContext()).ToChecked());
+        }
+
+        return res;
+    }
+};
+const std::vector<float> ArrayCvt::kFallbackV = {};
+
+class JSExprManagerImpl : public skottie::ExpressionManager
+{
+public:
+    JSExprManagerImpl(v8::Isolate *isolate, v8::Local<v8::Object> impl)
+        : isolate_(isolate), impl_this_(isolate, impl)
+    {
+        v8::Local<v8::Context> ctx = isolate->GetCurrentContext();
+#define GETPROP(gstore, key)            \
+        {                               \
+            v8::Local<v8::Value> v;     \
+            if (!impl->Get(ctx, v8::String::NewFromUtf8Literal(isolate, key)).ToLocal(&v))  \
+                g_throw(TypeError, "Missing property `" key "` in ExpressionManager impl"); \
+            if (!v->IsFunction())       \
+                g_throw(TypeError, "Property `" key "` must be a function"); \
+            gstore.Reset(isolate_, v.As<v8::Function>());                    \
+        }
+
+        GETPROP(func_create_number_expr_, "createNumberExpressionEvaluator")
+        GETPROP(func_create_str_expr_, "createStringExpressionEvaluator")
+        GETPROP(func_create_arr_expr_, "createArrayExpressionEvaluator")
+#undef GETPROP
+    }
+
+    template<typename T, typename ValueCvtFunctor>
+    sk_sp<skottie::ExpressionEvaluator<T>>
+    Invoke(v8::Local<v8::Function> func, const char *expr)
+    {
+        v8::Local<v8::Context> ctx = isolate_->GetCurrentContext();
+        v8::Local<v8::Value> args[] = {
+            v8::String::NewFromUtf8(isolate_, expr).ToLocalChecked()
+        };
+
+        v8::Local<v8::Value> ret;
+        if (func->Call(ctx, impl_this_.Get(isolate_), 1, args).ToLocal(&ret))
+            return nullptr;
+
+        if (!ret->IsFunction())
+            return nullptr;
+
+        return sk_make_sp<JSExprEvaluatorImplT<T, ValueCvtFunctor>>(
+                isolate_, ret.As<v8::Function>());
+    };
+
+    sk_sp<skottie::ExpressionEvaluator<float>>
+    createNumberExpressionEvaluator(const char *expression) override
+    {
+        v8::HandleScope scope(isolate_);
+        return Invoke<float, NumberCvt>(
+                func_create_number_expr_.Get(isolate_), expression);
+    }
+
+    sk_sp<skottie::ExpressionEvaluator<SkString>>
+    createStringExpressionEvaluator(const char *expression) override
+    {
+        v8::HandleScope scope(isolate_);
+        return Invoke<SkString, StringCvt>(
+                func_create_str_expr_.Get(isolate_), expression);
+    }
+
+    sk_sp<skottie::ExpressionEvaluator<std::vector<float>>>
+    createArrayExpressionEvaluator(const char *expression) override
+    {
+        v8::HandleScope scope(isolate_);
+        return Invoke<std::vector<float>, ArrayCvt>(
+                func_create_arr_expr_.Get(isolate_), expression);
+    }
+
+private:
+    v8::Isolate *isolate_;
+    v8::Global<v8::Object> impl_this_;
+    v8::Global<v8::Function> func_create_number_expr_;
+    v8::Global<v8::Function> func_create_str_expr_;
+    v8::Global<v8::Function> func_create_arr_expr_;
+};
+
 } // namespace anonymous
 
 AnimationBuilderWrap::AnimationBuilderWrap(uint32_t flags)
@@ -225,7 +380,13 @@ v8::Local<v8::Value> AnimationBuilderWrap::setPrecompInterceptor(v8::Local<v8::V
 
 v8::Local<v8::Value> AnimationBuilderWrap::setExpressionManager(v8::Local<v8::Value> manager)
 {
-    // TODO(sora): implement this
+    v8::Isolate *isolate = v8::Isolate::GetCurrent();
+    if (!manager->IsObject())
+        g_throw(TypeError, "Argument `manager` must be an object");
+
+    builder_.setExpressionManager(sk_make_sp<JSExprManagerImpl>(
+            isolate, manager.As<v8::Object>()));
+
     return ReturnThis();
 }
 
